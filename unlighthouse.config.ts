816c3746a75a35f570d647df0449641f98d733a4
Florian Rohrer <dev@florianrohrer.ch>
2024-01-03 22:16:33 +0100

    setup unlighthouse

---
+++ unlighthouse.config.ts
@@ -0,0 +1,11 @@
+export default {
+  site: "jso-crescendo.ch",
+  debug: true,
+  cache: false,
+  routerPrefix: "/unlighthouse",
+  scanner: {
+    device: "mobile",
+    skipJavascript: false,
+  },
+  ci: { buildStatic: true },
+};
